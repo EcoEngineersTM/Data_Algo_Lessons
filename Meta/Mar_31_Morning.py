29e626ce206d73b96082e4e0986bae62996bebe4
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-03-31 08:46:43 -0400

    mar_31 two sum review

---
+++ Meta/Mar_31_Morning.py
@@ -0,0 +1,16 @@
+
+# given nums = [4, 7, 2, 9], and targetSum = 
+'''
+given nums = [4, 7, 2, 9], and targetSum = 11
+create a function which return two numbers that sum up to targetSum
+
+Three Different Solution. make sure explain time & space complexity
+'''
+
+# Brute Force (Enumeration)
+
+
+# Optmial Solution 1
+
+
+# Optimal Solution 2
