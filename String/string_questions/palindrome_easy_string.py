7ba2b15974985f69cd1b8f1511f27b6ec231d653
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-04-04 00:37:26 -0400

    palindrome solution added

--- String/string_questions/palindrome_easy_string.py
+++ String/string_questions/palindrome_easy_string.py
@@ -23,3 +23,44 @@ Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
 '''
+
+# method 1 Two Pointers
+
+'''
+Python String isalnum()
+The isalnum() method returns True if all characters in the string are alphanumeric (either alphabets or numbers). 
+If not, it returns False.
+
+
+1. create output list to return
+2. create two pointers, left = 0 & right = s[len(s)]
+3. while left < right:
+    check if left != right:
+        return False
+4. Return True
+'''
+
+# Time: O(n)
+# Space: O(1)
+def palindromeImmutable(s):
+    left = 0 
+    right = len(s)-1
+
+    while left < right:
+        if not s[left].isalnum():
+            left += 1
+            continue
+        if not s[right].isalnum():
+            right -= 1
+            continue
+
+        if s[left].lower() != s[right].lower():
+            return False
+
+        left += 1
+        right -= 1
+
+
+s = 'noon'
+
+print(print(palindromeImmutable(s)))
