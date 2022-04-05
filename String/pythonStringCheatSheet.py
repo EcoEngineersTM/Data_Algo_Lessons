1b8f44515ecc0860c4b73dd2f3bda85890fd2726
ans9611 <62779373+ans9611@users.noreply.github.com>
2022-04-04 20:57:00 -0400

    added resources to research

---
+++ String/pythonStringCheatSheet.py
@@ -0,0 +1,11 @@
+# Python String Methods
+
+'''
+Python String isalnum()
+
+The isalnum() method returns True 
+if all characters in the string are either alphabets or numbers
+If not, it returns False.
+
+string.isalnum()
+'''
