a6ff32c926f2dfeef9d48ce7258a19ac02104cfe
TaekYoung <ltyiz07@gmail.com>
2022-04-12 00:47:08 +0900

    on progress with graph, breadth first search

---
+++ Meta/Apr_12_Night.py
@@ -0,0 +1,29 @@
+#/user/bin/env python
+"""algorith practice"""
+
+__author__ = "TaekYoung Lee"
+__email__ = "ltyiz07@gmail.com"
+
+
+
+"""
+To get shortest path from graph
+
+sample
+@a ---- @b ---- @d ----- @e --- @g
+                 |  \          /
+                 |   \-- @f --/
+                @c
+from @a
+    distance 1: @b
+    distance 2: @d
+    distance 3: @c, @e, @f
+    distance 4: @g
+
+so te shortest path from a to g is 4
+"""
+
+if __name__ == "__main__":
+    pass
+
+    
